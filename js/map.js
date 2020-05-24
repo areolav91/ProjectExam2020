a07e8d5e793abf759c66420409cd84930e445e9d
AreOlav <57764765+areolav91@users.noreply.github.com>
2020-05-24 22:59:44 +0200

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,12 +1,12 @@
-function initMap() {
+function homeMap() {
 	var options = {
 		zoom: 14,
 		center: { lat: 38.882938, lng: -77.016197 },
 	};
 
-	var map = new google.maps.Map(document.getElementById("map"), options);
+	var map = new google.maps.Map(document.getElementById("home"), options);
 
-	var marker = new google.maps.Marker({
+	var needle = new google.maps.Marker({
 		position: { lat: 38.882938, lng: -77.016197 },
 		map: map,
 	});
